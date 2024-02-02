112c6e029e6e547ed24ca7462c01a52e4b112ea6
Igor Fedyanin <logowaz25@yandex.ru>
2024-02-02 03:04:06 +0300

    новая попытка2

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ const cors = require('cors');
 const errorHandler = require('./middlewares/errorHandler');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { PORT, MONGO_DB } = require('./utils/config');
-// const { PORT = 3000 } = process.env;
+
 const app = express();
 
 app.use(express.json());
@@ -25,6 +25,7 @@ app.use(cors({
   origin: ['http://localhost:3000', 'http://localhost:3001', 'https://localhost:3000', 'https://localhost:3001', 'http://localhost:5173', 'https://localhost:5173', 'https://logowaz25.nomoredomainsmonster.ru', 'http://logowaz25.nomoredomainsmonster.ru',
     'http://logowaz25back.nomoredomainsmonster.ru', 'https://logowaz25back.nomoredomainsmonster.ru'],
   credentials: true,
+  allowedHeaders: ['Content-Type', 'Authorization', 'Access-Control-Allow-Origin'],
 }));
 
 app.get('/crash-test', () => {
